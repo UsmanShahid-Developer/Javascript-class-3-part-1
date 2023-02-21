cb191bbeba9b348462c226af014f01ad46c79a38
UsmanShahid-Developer <usmanshahidite@gmail.com>
2023-02-21 21:04:39 +0500

    class-3-part-1

---
+++ practice.js
@@ -0,0 +1,13 @@
+    document.getElementById("alertString").onclick = function (){
+        alert("I Am Muahmmad Usman Shahid.")
+        document.getElementById('statement').innerHTML = "I am usman shahid"
+    }
+    document.getElementById("number1").onclick = function (){
+        alert("03027175981")
+        document.getElementById('statement').innerHTML = "My Phone Number is 03027175981."
+    }
+
+    var firstname="usman"
+    var secondname="shahid"
+    var fullname =firstname+ " "+ secondname
+    document.write(fullname)
